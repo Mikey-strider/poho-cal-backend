c1f4588ba0a861562c6f4c74f8d93dca89d3d8cc
Dromen2001 <dromen2001@gmail.com>
2026-04-24 17:54:38 -0700

    building events routing

--- controllers/events.js
+++ controllers/events.js
@@ -3,7 +3,21 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    res.json({ message: 'Create Route' });
+    try {
+        const createdEvent = await Event.create(req.body);
+        res.status(201).json(createdEvent);
+    } catch (error) {
+        res.status(500).json({ error: error.message});
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const foundEvents = await Event.find();
+        res.status(200).json(foundEvents);
+    } catch (error) {
+        res.status(500).json({ error: error.message});
+    }
 });
 
 
